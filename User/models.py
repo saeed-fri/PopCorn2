c017a9e2f76fb86d6ef8b20ffad3a186cfcf3111
meprime <sadra27alimi@gmail.com>
2015-05-31 19:09:40 +0430

    User model

--- User/models.py
+++ User/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
+"""
 class User(models.Model):
     username = models.CharField(max_length=30)
     alias = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     email = models.EmailField(max_length=70)
     birthday = models.DateTimeField()
+"""
+
+class UserProfile(models.Model):  #SiteUser is an extension of User
+    user = models.OneToOneField(User)
+    alias = models.CharField(max_length=50)
+    birthday = models.DateField()
